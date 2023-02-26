34838d5a2d7fa5cfb570accb45f942d8d3823fc5
madanadra <mlaksindra@gmail.com>
2023-02-26 16:05:47 +0700

    v5

--- pages/signin.js
+++ pages/signin.js
@@ -17,9 +17,9 @@ export default function Signin() {
     return (
         !session &&
         <Layout title='Sign in'>
-            <div className="grid gap-y-4 py-5 px-3.5 md:py-9 md:px-8 w-full max-w-sm mx-auto">
+            <div className="grid gap-y-5 py-5 px-3.5 md:py-9 md:px-8 w-full max-w-sm mx-auto">
                 <h1 className='text-3xl text-center font-medium font-logo mb-8 leading-none'>Indragram</h1>
-                <div className='grid gap-y-3 rounded-lg border border-slate-300 bg-slate-100 p-3'>
+                <div className='grid gap-y-3.5 rounded-lg border border-slate-300 bg-slate-100 p-3.5'>
                     <div className='relative'>
                         <input type='text' id='username' placeholder=' ' 
                         className='pt-5 pb-2 px-2.5 border border-slate-300 focus:border-blue-500 bg-[#fff] 
@@ -28,7 +28,7 @@ export default function Signin() {
                         className='text-slate-500 leading-none top-2 text-xs 
                         peer-focus:leading-none peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500 
                         peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base
-                        duration-75 absolute inset-x-2.5 pointer-events-none '>Username or email</label>
+                        duration-75 absolute inset-x-2.5 pointer-events-none'>Username or email</label>
                     </div>
                     <div className='relative'>
                         <input type={showPass ? 'text' : 'password'} id='password' placeholder=' ' 
@@ -49,7 +49,7 @@ export default function Signin() {
                             </div>
                         </div>
                     </div>
-                    <h1 className='py-2 px-3 bg-blue-500 text-center text-slate-50 text-sm font-bold rounded-full cursor-pointer my-1'>Sign in</h1>
+                    <h1 className='py-2 px-3 bg-blue-500 text-center text-slate-50 text-sm font-bold rounded-full cursor-pointer my-1.5'>Sign in</h1>
                     <h1 className='text-xs font-semibold text-blue-500 w-max mx-auto cursor-pointer leading-none'>Forgot password?</h1>
                 </div>
                 <div className='flex items-center gap-x-2'>
